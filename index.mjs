5f59e796e7cb59964458355cb006f86dd5e08de3
Aqeel Ahmed <send2aqeelahmed@gmail.com>
2022-02-01 21:14:23 +0500

    bug fix

--- index.mjs
+++ index.mjs
@@ -1,6 +1,6 @@
 import express from 'express'
 const app = express()
-const port = 3000
+const PORT = process.env.PORT;
 
 app.get('/', (req, res) => {
   res.send('I am express.js')
@@ -10,6 +10,6 @@ app.get('/food', (req, res) => {
     res.send('Hello, I am Food using express.js')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
 })
